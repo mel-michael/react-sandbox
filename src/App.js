7f7755b6079d570ff6967e34384e95c50716244a
Mel Michael <mel.michael007@gmail.com>
2019-01-18 16:55:08 +0100

    Refactor and code clean up for file input form

--- src/App.js
+++ src/App.js
@@ -1,67 +1,21 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import NameForm from './NameForm';
-// import MessageForm from './MessageForm';
-// import SelectTag from './SelectTag';
-// import FileInputForm from './FileInputForm';
+import NameForm from './NameForm';
+import MessageForm from './MessageForm';
+import SelectTag from './SelectTag';
+import FileInputForm from './FileInputForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <NameForm /> */}
-          {/* <MessageForm /> */}
-          {/* <SelectTag /> */}
+          <NameForm /> 
+          <MessageForm />
+          <SelectTag />
           <FileInputForm />
         </header>
-      </div>
-    );
-  }
-}
-
-class FileInputForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submitted: null
-    }
-    this.singleFileInput = React.createRef();
-    this.multipleFileInput = React.createRef();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleFile = this.handleFile.bind(this);
-  }
-
-  handleFile() {
-    console.log('>>', this.singleFileInput.current.files)
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    let fileNames = `${this.singleFileInput.current.files[0].name}`;
-    const multipleFiles = this.multipleFileInput.current.files;
-    Object.keys(multipleFiles).forEach(file => {
-      fileNames += `, ${multipleFiles[file].name}`;
-    });
-    this.setState({ submitted: fileNames })
-  }
-
-  render() {
-    const { submitted } = this.state;
-    return (
-      <div>
-        File Input Tag
-        <form onSubmit={this.handleSubmit}>
-          <p> Select One File: 
-            <input type="file"  ref={this.singleFileInput} onChange={this.handleFile} />
-          </p>
-          <p> Select Multiple File: 
-            <input type="file" multiple ref={this.multipleFileInput} />
-          </p>
-          <button type="submit">Submit</button>
-        </form>
-        {submitted && <p>Submitted file names are:  {submitted}</p>}
       </div>
     );
   }
