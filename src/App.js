2ea452973a7ac7361d2f694a74dcc25d3a6d6871
Mel Michael <mel.michael007@gmail.com>
2019-01-21 16:07:37 +0100

    Comment redundant code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
-import './App.css';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  // Redirect
+} from 'react-router-dom';
 
+import './App.css';
 // import BasicRouting from './BasicRouting';
 
 class App extends Component {
